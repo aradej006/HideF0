e832eba3fcd55d5a6af9db9247f2800ae9c82c7d
aradej006 <aradej006@gmail.com>
2019-02-24 09:58:06 +0100

    print histogramData

--- src/main/java/pl/pw/radeja/Config.java
+++ src/main/java/pl/pw/radeja/Config.java
@@ -4,21 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-final class Config {
-    static final int NUMBER_OF_THREADS = 16;
-    static final boolean CALCULATE_ALLOW_PLACES = true;
-    static final boolean DECODE_FILES = true;
-    static final boolean CALCUALTE_PESQ = false;
-    static final boolean PRINT_WEKA_FILES = false;
-    static final boolean PRINT_WEKA_VECTOR_FILES = false;
-    static final boolean PRINT_CALCULATED_THRESHOLDS = false;
-    static final boolean PRINT_HISTOGRAM = false;
-    static final HideF0Type HIDE_F0_TYPE = HideF0Type.FIRST_LAST;
+public final class Config {
+    public static final int NUMBER_OF_THREADS = 8;
+    public static final boolean CALCULATE_ALLOW_PLACES = true;
+    public static final boolean DECODE_FILES = false;
+    public static final boolean CALCUALTE_PESQ = false;
+    public static final boolean PRINT_WEKA_FILES = false;
+    public static final boolean PRINT_WEKA_VECTOR_FILES = false;
+    public static final boolean PRINT_CALCULATED_THRESHOLDS = false;
+    public static final boolean PRINT_HISTOGRAM = true;
+    public static final HideF0Type HIDE_F0_TYPE = HideF0Type.FIRST_LAST;
 
-    static final List<Integer> THRESHOLDS = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 110, 127);
+    public static final List<Integer> THRESHOLDS = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 110, 127);
+//public static final List<Integer> THRESHOLDS = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 //    static final List<Integer> THRESHOLDS = Arrays.asList(0);
 
-    static List<String> getSamples() {
+    public static List<String> getSamples() {
         final String baseMalePath = "D:/PracaMgr/master-thesis/TIMIT_M/";
         final String baseFemalePath = "D:/PracaMgr/master-thesis/TIMIT_F/";
 //        final int maleLimit = 2;
@@ -35,7 +36,7 @@ final class Config {
         return samples;
     }
 
-    enum HideF0Type {
+    public enum HideF0Type {
         FIRST_LAST("FL"),
         FIRST_LAST_RAND("FLRand"),
         FIRST_FIRST("FF");
--- src/main/java/pl/pw/radeja/statistic/BytesHistogramPrinter.java
+++ src/main/java/pl/pw/radeja/statistic/BytesHistogramPrinter.java
@@ -11,22 +11,22 @@ import java.util.stream.Collectors;
 public class BytesHistogramPrinter {
 
     public static void printHistograms(List<BitsCollector> bitsCollectors) {
-        Map<String, List<BitsCollector>> collect = bitsCollectors.stream()
-                .collect(Collectors.groupingBy(BitsCollector::getSampleName, Collectors.toList()));
-        Map<String, Map<Integer, Map<Integer, Long>>> sampleNameToThresholdToValueToCount = collect.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, v -> v.getValue().stream().collect(Collectors.toMap(BitsCollector::getThreshold, BitsCollectorParser::getHistogramData))));
-        sampleNameToThresholdToValueToCount.forEach((sampleName, thToValToCnt) -> {
-            List<Integer> thresholds = new ArrayList<>(thToValToCnt.keySet());
-            Collections.sort(thresholds);
+        Map<Integer, List<BitsCollector>> collect = bitsCollectors.stream()
+                .collect(Collectors.groupingBy(BitsCollector::getThreshold, Collectors.toList()));
+        Map<Integer, Map<String, Map<Integer, Long>>> thresholdToSampleNameToValueToCount = collect.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, v -> v.getValue().stream().collect(Collectors.toMap(BitsCollector::getSampleName, BitsCollectorParser::getHistogramData))));
+        thresholdToSampleNameToValueToCount.forEach((threshold, thToValToCnt) -> {
+            List<String> sampleNames = new ArrayList<>(thToValToCnt.keySet());
+            Collections.sort(sampleNames);
             List<Integer> values = new ArrayList<>(thToValToCnt.values().stream().findAny().orElseThrow(() -> new IllegalArgumentException("No data")).keySet());
             Collections.sort(values);
 
             System.out.println("\n\n\n");
-            System.out.println("HistogramData for " + sampleName);
-            System.out.println("Values\t" + thresholds.stream().map(Object::toString).collect(Collectors.joining("\t")));
+            System.out.println("HistogramData for Th=" + threshold);
+            System.out.println("Values\t" + sampleNames.stream().map(Object::toString).collect(Collectors.joining("\t")));
             values.forEach(v -> {
                 List<String> vals = new ArrayList<>();
                 vals.add(v.toString());
-                thresholds.forEach(th -> vals.add(thToValToCnt.get(th).get(v).toString()));
+                sampleNames.forEach(th -> vals.add(thToValToCnt.get(th).get(v).toString()));
                 System.out.println(String.join("\t", vals));
             });
             System.out.println("\n\n\n");
